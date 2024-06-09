4efa27ca85bf04161d6872d969cf19371ea76404
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-09 13:55:18 +0330

    Final cleanup
    https://react.dev/learn/tutorial-tic-tac-toe#final-cleanup

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import Board, { Squares } from "../Board";
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState<Squares[]>([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = Number.isInteger(currentMove / 2)
   const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares: Squares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   };
 
   const moves = history.map((squares, move) => {
@@ -28,7 +27,6 @@ const Game = () => {
 
   const jumpTo = (nextMove: number) => {
     setCurrentMove(nextMove);
-    setXIsNext(Number.isInteger(nextMove / 2));
   };
 
   return (
